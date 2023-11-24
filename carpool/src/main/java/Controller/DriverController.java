62c1c89c10d01c89b1300b1f04ee3d0658b34082
saadshuraim <saadchacu@gmail.com>
2023-11-24 15:28:02 +0500

    Updated Driver and Rider controller class

--- carpool/src/main/java/Controller/DriverController.java
+++ carpool/src/main/java/Controller/DriverController.java
@@ -1,4 +1,15 @@
 package Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DriverController {
+
+    @GetMapping("/driver")
+    public String driver() {
+        return "driver";
+    }
+
+    // Add methods for driver-related operations as needed
 }
--- carpool/src/main/java/Controller/RiderController.java
+++ carpool/src/main/java/Controller/RiderController.java
@@ -1,4 +1,16 @@
 package Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RiderController {
+
+    @GetMapping("/rider")
+    public String rider() {
+        return "rider";
+    }
+
+    // Add methods for rider-related operations as needed
 }
+
